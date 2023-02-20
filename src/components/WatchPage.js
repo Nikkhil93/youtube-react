15a9477861a679442dd1ea94748a494ec4c06d66
Nikhil Kumar <nikhil.kumar@siemens-healthnieers.com>
2023-02-20 16:17:54 +0530

    enabling autoPlay

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -41,7 +41,7 @@ const WatchPage = () => {
       <div className="pr-5 h-screen w-4/6 mt-2">
         <iframe
         className=" h-4/6 w-full"
-          src={"https://www.youtube.com/embed/" + searchParams.get("v") +'?autoplay=0'}
+          src={"https://www.youtube.com/embed/" + searchParams.get("v") +'?autoplay=1'}
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
